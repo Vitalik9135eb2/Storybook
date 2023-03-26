8129e5580fdffcea15b34ff7474524f52100a748
Vitalik <05vp94@mail.ru>
2023-03-26 20:17:40 +0300

    add stories react memo

---
+++ src/stories/ReactMemo/ReactMemo.stories.tsx
@@ -0,0 +1,31 @@
+import React, {useState} from "react";
+
+export default {
+    title: "React memo demo"
+}
+
+const Counter = (props: { count: number }) => {
+    return <div>{props.count}</div>
+}
+
+const Users = (props: { users: string[] }) => {
+    console.log("users update")
+    return <div>{
+        props.users.map((u, i) => <div key={Math.random()}>{u} </div>)
+    }</div>
+}
+
+const UsersMemo = React.memo(Users)
+
+export const Example1 = () => {
+    const [counter, setCounter] = useState<number>(0)
+    const [users, setUsers] = useState<string[]>(["Minsk", "Vitemsk", "Kiev"])
+
+
+    return <>
+        <button onClick={() => {setCounter(counter+1)}}>+</button>
+        <Counter count={counter}/>
+        <UsersMemo users={users}/>
+    </>
+
+}
