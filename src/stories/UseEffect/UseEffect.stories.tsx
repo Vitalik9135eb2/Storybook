90db2b67945adadd8afd04097478415b35eee0a2
Vitali.Palcheuski <05vp94@mail.ru>
2023-05-01 11:57:18 +0300

    done all lessons for useEffect

--- src/stories/UseEffect/UseEffect.stories.tsx
+++ src/stories/UseEffect/UseEffect.stories.tsx
@@ -38,15 +38,4 @@ export const Example1 = () => {
 
 
 
-export const Clock = () => {
-    const [date, setDate] = useState(new Date())
-
-    return(
-        <div>
-            <span>{date.getHours()}</span>:
-            <span>{date.getMinutes()}111</span>:
-            <span>{date.getSeconds()}</span>
-        </div>
-    )
-}
 
